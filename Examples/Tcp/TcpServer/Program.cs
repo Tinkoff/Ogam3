cde6e01efa0afa610b5a6dc942ffef8e9eab3605
Zhukov Anton Andreevich <a.zhukov1@tinkoff.ru>
2018-01-19 15:26:56 +0300

    fix server

--- Examples/Tcp/TcpServer/Program.cs
+++ Examples/Tcp/TcpServer/Program.cs
@@ -12,12 +12,6 @@ namespace TcpServer {
     class Program {
         static void Main(string[] args) {
             var srv = new OTcpServer(1010);
-            srv.Evaluator.DefaultEnviroment.Define(new Symbol("get-context-tcp-client"), new Func<dynamic>(() => {
-                var client = Thread.GetData(Thread.GetNamedDataSlot("context-tcp-client"));
-
-                return client != null;
-            }));
-
             Console.WriteLine("OK");
             Console.ReadLine();
         }
