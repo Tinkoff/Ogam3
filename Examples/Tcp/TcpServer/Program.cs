ab2b5b5e1732eadb6614449c4a6a220161c3e37c
Zhukov Anton Andreevich <a.zhukov1@tinkoff.ru>
2018-05-03 16:20:22 +0300

    Copyright text was added

--- Examples/Tcp/TcpServer/Program.cs
+++ Examples/Tcp/TcpServer/Program.cs
@@ -1,19 +1,23 @@
-﻿using System;
-using System.CodeDom;
-using System.CodeDom.Compiler;
-using System.Collections.Concurrent;
+﻿/*
+ * Copyright © 2018 Tinkoff Bank
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net.Sockets;
-using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using CommonInterface;
-using Ogam3.Lsp;
 using Ogam3.Network.Tcp;
 using Ogam3.Utils;
 
