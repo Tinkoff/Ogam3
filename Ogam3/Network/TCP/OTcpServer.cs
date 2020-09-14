c9228def1dbb362886f041feaadfa580a701cd4b
d.grzhendko <d.grzhendko@tinkoff.ru>
2020-09-14 17:41:41 +0300

    add ability to disable logging

--- Ogam3/Network/TCP/OTcpServer.cs
+++ Ogam3/Network/TCP/OTcpServer.cs
@@ -3,16 +3,13 @@ using Ogam3.Lsp;
 using Ogam3.Lsp.Generators;
 using Ogam3.Network.Tcp;
 using Ogam3.TxRx;
-using Ogam3.Utils;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Ogam3.Network.TCP {
     public class OTcpServer {
@@ -21,13 +18,16 @@ namespace Ogam3.Network.TCP {
         public const uint BufferSize = 1048576;
         public Evaluator Evaluator;
 
-        public static Action<string> Log = Console.WriteLine;
+        private static readonly Action<string> LogAction = Console.WriteLine;
+        private readonly bool _isLogEnabled;
 
         private readonly QueryInterface _queryInterface;
 
         public OTActorEngine Actors;
 
-        public OTcpServer(int port, Evaluator evaluator = null, bool isAutoStartListener = true) {
+        public OTcpServer(int port, Evaluator evaluator = null, bool isAutoStartListener = true, bool isLogEnabled = true) {
+            _isLogEnabled = isLogEnabled;
+            
             Evaluator = evaluator ?? new Evaluator();
 
             Actors = new OTActorEngine();
@@ -83,21 +83,58 @@ namespace Ogam3.Network.TCP {
 
             server.ReceivedData += (rap, data) => {
                 //Console.WriteLine("Main Thread: {0}", Thread.CurrentThread.ManagedThreadId);
-                Actors.EnqueueData(new OTContext() { // Handle in other thread
+                var otContext = new OTContext() { // Handle in other thread
                     Context = rap,
                     Data = data,
                     TcpClient = client,
                     ReClient = new ReClient(server, Evaluator, _queryInterface),
                     Evaluator = Evaluator,
-                    DataTransfer = server,
-                    Callback = HandleRequest
-                });
+                    DataTransfer = server
+                };
+                if (_isLogEnabled) {
+                    otContext.Callback = HandleRequestLogging;    
+                }
+                else {
+                    otContext.Callback = HandleRequest;    
+                }
+                Actors.EnqueueData(otContext);
             };
 
             server.StartReaderLoop();
         }
 
         private static void HandleRequest(OTContext context) {
+            if (context.DataTransfer.HandlResp(context.Context, context.Data)) {
+                // handled as result of request
+                return;
+            }
+
+            SetContextObj(ContextId, context);
+
+            var response = new byte[0];
+            try {
+                var receive = BinFormater.Read(new MemoryStream(context.Data), context.SymbolTable);
+                var res = context.Evaluator.EvlSeq(receive, false);
+                if (res != null) {
+                    response = BinFormater.Write(res, context.SymbolTable).ToArray();
+                }
+            } catch (Exception e) {
+                var ex = e;
+                var sb = new StringBuilder();
+                while (ex != null) {
+                    sb.AppendLine(ex.Message);
+                    ex = ex.InnerException;
+                }
+                var msg = sb.ToString();
+                LogAction?.Invoke(msg);
+                response = BinFormater.Write(new SpecialMessage(msg), context.SymbolTable).ToArray();
+            }
+
+            // send response to client
+            context.DataTransfer.WriteData(response, context.Context);
+        }
+
+        private static void HandleRequestLogging(OTContext context) {
             if (context.DataTransfer.HandlResp(context.Context, context.Data)) {
                 // handled as result of request
                 return;
@@ -116,7 +153,7 @@ namespace Ogam3.Network.TCP {
                 var res = context.Evaluator.EvlSeq(receive, false);
 
                 transactLog.AppendLine($">> {res}");
-                Log?.Invoke(transactLog.ToString().Trim());
+                LogAction?.Invoke(transactLog.ToString().Trim());
 
                 if (res != null) {
                     responce = BinFormater.Write(res, context.SymbolTable).ToArray();
@@ -129,13 +166,14 @@ namespace Ogam3.Network.TCP {
                     sb.AppendLine(ex.Message);
                     ex = ex.InnerException;
                 }
-                Log?.Invoke(transactLog.ToString());
+                LogAction?.Invoke(transactLog.ToString());
                 responce = BinFormater.Write(new SpecialMessage(sb.ToString()), context.SymbolTable).ToArray();
             }
 
             // send responce to client
             context.DataTransfer.WriteData(responce, context.Context);
         }
+
 
         private static object GetContextObj(string id) {
             return Thread.GetData(Thread.GetNamedDataSlot(id));
